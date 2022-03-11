2c5d9f1a4afa3f1baf2ce7cd525d6b291c9f6752
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-03-11 11:18:10 +0300

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,27 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
 public class App {
-}
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+    public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+        staticFileLocation("/public");
+
+        //home
+        get("/",(request, response) -> {
+            Map<String,Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model,"index.hbs");
+        },new HandlebarsTemplateEngine());
+}}
