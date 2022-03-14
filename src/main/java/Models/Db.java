8bd69545c2952641941772838002515c3b0aff5e
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-03-14 23:28:51 +0300

    Setup Db Connection

--- src/main/java/Models/Db.java
+++ src/main/java/Models/Db.java
@@ -3,7 +3,7 @@ package Models;
 import org.sql2o.Sql2o;
 
 public class Db {
-    String connectionString = "jdbc:postgresql://ec2-3-216-221-31.compute-1.amazonaws.com:5432/daq0dss1uqjbpp"; //!
-    Sql2o sql2o = new Sql2o(connectionString, "laynxhqaenrxke", "0e0e26df811d068f818734dcc3eb18a0ae91836ddf34c621d55f6a84e1fceb62"); //!
+    static String connectionString = "jdbc:postgresql://ec2-3-216-221-31.compute-1.amazonaws.com:5432/daq0dss1uqjbpp"; //!
+    static Sql2o sql2o = new Sql2o(connectionString, "laynxhqaenrxke", "0e0e26df811d068f818734dcc3eb18a0ae91836ddf34c621d55f6a84e1fceb62"); //!
 }
 
